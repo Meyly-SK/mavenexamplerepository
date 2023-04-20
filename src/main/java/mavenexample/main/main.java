fa8db98eb932dfa5b68f303b39f6cda6aba6dba7
Meyly-SK <meylymerma03@gmail.com>
2023-04-19 20:51:27 -0500

    añadido el banco bcp

--- src/main/java/mavenexample/main/main.java
+++ src/main/java/mavenexample/main/main.java
@@ -10,14 +10,27 @@ public class main {
 		// TODO Auto-generated method stub
 		EntityManager em = Conn.getInstancia().getFactory().createEntityManager();
 		
-		Banco bancoSaga = new Banco();
+		/* Banco bancoSaga = new Banco(); 
 		bancoSaga.setBancoNombre("Saga");
 		
 		em.getTransaction().begin();
 		em.persist(bancoSaga);
 		em.getTransaction().commit();
 		
+		System.out.println("Se creo un nuevo banco"); */
+		
+		
+		Banco bcpBanco = new Banco();
+		bcpBanco.setBancoNombre("BCP");
+		
+		em.getTransaction().begin();
+		em.persist(bcpBanco);
+		em.getTransaction().commit();
+		System.out.println("Terminado");
+		
+		Banco bcpResultado = em.find(Banco.class,2);
 		System.out.println("Se creo un nuevo banco");
+		em.close();
 	}
 
 }
